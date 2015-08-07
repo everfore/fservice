12994a779dbd92c5a66ddd1e752be54b7772ce0c
shaalx <1316011195@qq.com>
2015-08-07 10:25:57 +0800

    task

--- main.go
+++ main.go
@@ -4,14 +4,42 @@ import (
 	"github.com/astaxie/beego"
 	"github.com/everfore/fservice/controllers"
 	_ "github.com/everfore/fservice/routers"
+
+	"fmt"
+	"github.com/astaxie/beego/toolbox"
 )
 
 func main() {
+	beego.EnableAdmin = true
+	beego.AdminHttpAddr = "localhost"
+	beego.AdminHttpPort = 8088
+
 	app := beego.Include(&controllers.FileServerController{})
 
 	app.Run()
 }
 
 func init() {
+
 	beego.SetStaticPath("/public", "static")
+
+	toolbox.AddHealthCheck("database", &DatabaseCheck{})
+	go a()
+}
+
+type DatabaseCheck struct {
+}
+
+func (c *DatabaseCheck) Check() error {
+	return nil
+}
+
+func a() {
+	tk := toolbox.NewTask("taska", "0/10 * * * * *", func() error { fmt.Println("hello world"); return nil })
+	err := tk.Run()
+	if err != nil {
+		beego.Error(err)
+	}
+	toolbox.AddTask("taska", tk)
+	toolbox.StartTask()
 }
