e9d865516d7b464534b4b695bd8087c7ab00f681
shaalx <1316011195@qq.com>
2015-08-07 09:09:46 +0800

    ignore

---
+++ routers/commentsRouter_github_com_everfore_fservice_controllers.go
@@ -0,0 +1,51 @@
+package routers
+
+import (
+	"github.com/astaxie/beego"
+)
+
+func init() {
+
+	beego.GlobalControllerRouter["github.com/everfore/fservice/controllers:FileServerController"] = append(beego.GlobalControllerRouter["github.com/everfore/fservice/controllers:FileServerController"],
+		beego.ControllerComments{
+			"Get",
+			`/`,
+			[]string{"get"},
+			nil})
+
+	beego.GlobalControllerRouter["github.com/everfore/fservice/controllers:FileServerController"] = append(beego.GlobalControllerRouter["github.com/everfore/fservice/controllers:FileServerController"],
+		beego.ControllerComments{
+			"LoadUpload",
+			`/upload`,
+			[]string{"get"},
+			nil})
+
+	beego.GlobalControllerRouter["github.com/everfore/fservice/controllers:FileServerController"] = append(beego.GlobalControllerRouter["github.com/everfore/fservice/controllers:FileServerController"],
+		beego.ControllerComments{
+			"Upload",
+			`/upload`,
+			[]string{"post"},
+			nil})
+
+	beego.GlobalControllerRouter["github.com/everfore/fservice/controllers:FileServerController"] = append(beego.GlobalControllerRouter["github.com/everfore/fservice/controllers:FileServerController"],
+		beego.ControllerComments{
+			"Show",
+			`/show`,
+			[]string{"get"},
+			nil})
+
+	beego.GlobalControllerRouter["github.com/everfore/fservice/controllers:FileServerController"] = append(beego.GlobalControllerRouter["github.com/everfore/fservice/controllers:FileServerController"],
+		beego.ControllerComments{
+			"LoadFile",
+			`/loadfile/*`,
+			[]string{"get"},
+			nil})
+
+	beego.GlobalControllerRouter["github.com/everfore/fservice/controllers:FileServerController"] = append(beego.GlobalControllerRouter["github.com/everfore/fservice/controllers:FileServerController"],
+		beego.ControllerComments{
+			"Download",
+			`/download/*`,
+			[]string{"get"},
+			nil})
+
+}
