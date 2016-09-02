9b61c8ab72f24c939da890e06b687ec0ae8418c5
toukii <toukii@yahoo.com>
2016-09-02 15:08:32 +0800

    auto

--- models/user.go
+++ models/user.go
@@ -11,7 +11,7 @@ type User struct {
 }
 
 func (u *User) Check(v *validation.Validation) {
-	if u.Name == "shaalx" && u.Passwd == "1234" {
+	if u.Name == "toukii" && u.Passwd == "1234" {
 		v.Clear()
 	} else {
 		v.SetError("login", "user is abnormal.")
